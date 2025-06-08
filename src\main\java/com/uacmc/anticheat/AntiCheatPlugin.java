70d08fdab6f44357871dec02912427ad9f393606
Gonsanz <lopezgarcia252345@gmail.com>
2025-06-08 10:06:38 -0300

    AntiCheatPlugin.java

--- src\main\java/com/uacmc/anticheat/AntiCheatPlugin.java
+++ src\main\java/com/uacmc/anticheat/AntiCheatPlugin.java
@@ -1,1 +1,28 @@
+package com.uacmc.anticheat;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AntiCheatPlugin extends JavaPlugin {
+
+    private SuspicionManager suspicionManager;
+    private MovementCheck movementCheck;
+    private CombatCheck combatCheck;
+    private XrayCheck xrayCheck;
+    private Logger logger;
+
+    @Override
+    public void onEnable() {
+        logger = new Logger(this);
+        suspicionManager = new SuspicionManager(this, logger);
+        movementCheck = new MovementCheck(this, suspicionManager, logger);
+        combatCheck = new CombatCheck(this, suspicionManager, logger);
+        xrayCheck = new XrayCheck(this, suspicionManager, logger);
+
+        Bukkit.getPluginManager().registerEvents(movementCheck, this);
+        Bukkit.getPluginManager().registerEvents(combatCheck, this);
+        Bukkit.getPluginManager().registerEvents(xrayCheck, this);
+
+        getLogger().info("AntiCheat avanzado habilitado.");
+    }
+}
